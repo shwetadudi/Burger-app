4510b51f384040558de8c5c585bd2fa745432577
shwetadudi <shweta.dudi92@gmail.com>
2020-04-25 13:35:37 +0530

    filter css updated

--- food-app/src/components/Filter/Filter.js
+++ food-app/src/components/Filter/Filter.js
@@ -4,11 +4,11 @@ const Filters = (props) => {
     const { filters, values, methods } = props;
     return <div className="filters col-md-3"> <h5>Refine Search</h5>{filters.map((filter) => {
         if (filter.values) {
-            return <div className="filter"> <strong>{filter.name} : </strong>
+            return <div className="filter"> <p><strong>{filter.name} : </strong></p><ul>
                 {
-                    filter.values.map((value) => <label htmlFor={value}>{value} <input onChange={(e) => methods[filter.name](value)} type="radio" name={filter.name} id={value} checked={values[filter.name] === value ? true : false} value={value}/></label>)
+                    filter.values.map((value) => <li><input onChange={(e) => methods[filter.name](value)} type="radio" name={filter.name} id={value} checked={values[filter.name] === value ? true : false} value={value}/><label htmlFor={value}> {value} </label></li>)
                 }
-            </div>
+            </ul></div>
         }
         return (<div><input onChange={(e) => methods[filter.name](e.target.value)} placeholder="Search" type="text" name={filter.name} value={values[filter.name]}/>
         <p>Search string: {methods.searchFilter}
