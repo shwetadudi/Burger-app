cd8ed6000cc2f50eee050ec05b6409408d171544
shwetadudi <shweta.dudi92@gmail.com>
2020-04-25 13:46:16 +0530

    css changes

--- food-app/src/components/Header/Header.js
+++ food-app/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div className="col-md-12 alert alert-primary"><h4 className="alert-heading">Order Food</h4></div>    
+    <div className="col-md-12 alert Header"><h4 className="alert-heading">Order Food</h4></div>    
 )
 
 export default Header;
--- food-app/src/components/OrderSummary/OrderSummary.js
+++ food-app/src/components/OrderSummary/OrderSummary.js
@@ -5,7 +5,9 @@ const orderSummary = (props) => (
     <ul className="list-group list-group-flush">
         <li className="list-group-item">Name : {props.name} </li>
         <li className="list-group-item">Price : {props.name} </li>
-        <button className="btn btn-info">Order Now</button>
+        <li className="list-group-item">Discount Applied : {props.discount} </li>
+        <button className="btn btn-info" onClick={props.orderSubmit}>Order Now</button><br/>
+        <p>Your Order will be delivered in {props.time} minutes!</p>
     </ul>
     </div>    
 )
